c1fe1c506358b5dc621acbdc1f9dcfe01af4a26d
superman <duonga2qp@gmail.com>
2022-07-22 22:32:04 +0700

    nop bai thuc hanh

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//
+//   </React.StrictMode>
+//
+// );
+
+const name = "Your name";
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    React.createElement("h1", { style: { textAlign: "center" } }, name)
 );
 
 // If you want to start measuring performance in your app, pass a function
