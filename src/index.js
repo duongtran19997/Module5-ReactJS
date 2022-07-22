c3e3ecbe9f563b3fa21a34c88f5cd5682d4da9e4
superman <duonga2qp@gmail.com>
2022-07-22 22:46:32 +0700

    nop bai thuc hanh 2

--- src/index.js
+++ src/index.js
@@ -14,12 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 //
 // );
 
-const name = "Your name";
+const name = "Quy Duong";
 
-root.render(
-    React.createElement("h1", { style: { textAlign: "center" } }, name)
-);
-
+root.render(<h1 style={{ textAlign: "center" }}>{name}</h1>);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
